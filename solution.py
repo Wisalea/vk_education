d6ac827ea5d0227663ce5e7fc9d42d56ecef4f3f
Svetlana <70641084+Wisalea@users.noreply.github.com>
2024-05-21 11:43:53 +0300

    модуль 2 задание 1

--- solution.py
+++ solution.py
@@ -1,29 +1,9 @@
-def remove_punctuation(input_string):
-    punctuation_chars = "!.,?;:#$%^&*()"
-    clean_string = ""
-    for char in input_string:
-        if char not in punctuation_chars:
-            clean_string += char
-    return clean_string
+def mid(data_in):
+    list_num = list(map(int, data_in.split()))
+    return sum(list_num) / len(list_num)
 
-
-def process_string(input_string):
-    input_string = remove_punctuation(input_string.lower())
-
-    words = input_string.split()
-
-    word_count = {}
-    for word in words:
-        if len(word) >= 5 and len(set(word)) >= 4:
-            word_count[word] = word_count.get(word, 0) + 1
-
-    filtered_words = [word for word, count in word_count.items() if count > 2]
-
-    filtered_words.sort()
-    return filtered_words
-
-
-text = input().replace("\\", " ")
-result = process_string(text)
-for i in result:
-    print(i)
+while True:
+    data = input()
+    if not data:
+        break
+    print(mid(data))
